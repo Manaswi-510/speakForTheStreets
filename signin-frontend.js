b8e431df53513d18c81f603d6bb386ba1341e855
Manaswi-510 <manaswisontakke@gmail.com>
2025-08-22 11:02:02 +0530

    Update signin-frontend.js

--- signin-frontend.js
+++ signin-frontend.js
@@ -1,9 +1,11 @@
+
+
 const form = document.querySelector("form");
 
 form.addEventListener("submit", async (e) => {
   e.preventDefault();
 
-  
+
   const email = form.username.value;  
   const password = form.password.value;
 
